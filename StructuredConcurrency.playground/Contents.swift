2a62b0a91737d4aecd171efaf515c448ba19e58c
AtesiT <atesit@mail.ru>
2026-02-15 12:26:54 +0300

    MADE: func to getWeather(async throws) that is called in Task {try await}

--- StructuredConcurrency.playground/Contents.swift
+++ StructuredConcurrency.playground/Contents.swift
@@ -1,5 +1,7 @@
 import Foundation
 
+//  MARK: - 1 TASK
+
 func fibonacci(of number: Int) -> Int {
     var first = 0
     var second = 1
@@ -17,6 +19,7 @@ fibonacci(of: 8)
 
 //  Данная функция аснхронная, так как на реализацию этой функции пришлось тратить много времени и она тормозила бы основной поток (из-за чего пользовательский интерфейс бы лагал\фризил). Мы отправляем эту задачу на фоновый режим (делаем асинхронно, параллельно всем процессам).
 func setFibonacciSequence(to numberCount: Int) async {
+    
     var numbers: [Int] = []
     
     for iteration in 1...numberCount {
@@ -27,8 +30,40 @@ func setFibonacciSequence(to numberCount: Int) async {
     print("The first \(numberCount) numbers in the fibonacci sequence are: \(numbers)")
 }
 
+//  2nd way.
+
+func setFibonacciSequenceUpd(to numberCount: Int) async {
+    let result = (1...numberCount).map(fibonacci)
+    print("The first \(numberCount) numbers in the fibonacci sequence are: \(result)")
+}
+
 Task {
     await setFibonacciSequence(to: 60)
 }
 
 //  В примере выше, мы вызвали обычную последовательную функцию внутри асинхронной функции
+
+
+//  MARK: - 2 TASK
+
+enum LocationError: Error {
+    case unknown
+}
+
+func getWeatherReadings(for city: String) async throws -> [Int] {
+    switch city {
+    case "Moscow":
+        return (1...10).map { _ in Int.random(in: 6...18) }
+    case "Saint Petersburg":
+        return (1...10).map { _ in Int.random(in: 3...13) }
+    case "Sochi":
+        return (1...10).map { _ in Int.random(in: -2...20) }
+    default:
+        throw LocationError.unknown
+    }
+}
+
+Task {
+    let readings = try await getWeatherReadings(for: "Sochi")
+    print("Readings are: \(readings)")
+}
